9ea35fdb029cee6cad51f8eb54b904d455a004ed
Emmanium <pradoe.ep@gmail.com>
2020-01-10 20:47:08 -0700

    got all tests passing for ancestor.py

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -7,26 +7,35 @@ def earliest_ancestor(ancestors, starting_node):
             vertices[pair[1]] = set()
         vertices[pair[1]].add(pair[0])
     print(vertices)
-    # establish visited cache, use a set
+    # declare a visited set to take away any redundant stack calls
     visited = set()
+    # visited lists holds tuple pairs,
     visited_list = []
     stack = []
     stack.append(starting_node)
+    # level keeps track of what level of the tree we are on (depth)
+    level = 0
     while len(stack) > 0:
+        level += 1
         # pop the top off
         vertex = stack.pop()
-        # if it hasn't been visited
+        if vertex in visited:
+            level -= 1
         if vertex not in visited:
-            # print and add it to visited
             visited.add(vertex)
-            visited_list.append(vertex)
-            # add all of its neighbors to the stack
-            print(vertex)
-            latest_ancestor = vertices.get(vertex)
-            if latest_ancestor:
-                for ancestor in latest_ancestor:
+            # checks to see if vertices dict has the equivalent key
+            # Further Clarification: Looping over a dictionary that doesn't have the correct key results in a ValueError
+            # Further Further Clarification: We don't want that happen because it stops the script from finishing running
+            latest_ancestor_vert = vertices.get(vertex)
+            if latest_ancestor_vert:
+                for ancestor in latest_ancestor_vert:
+                    # adds all of the connected ancestor nodes to the stack
                     stack.append(ancestor)
-    if len(visited_list) == 1:
+                    # the next line is so we can keep a track of what level of the tree we are on
+                    visited_list.append((level, ancestor))
+    if len(visited_list) == 0:
         return -1
+    elif len(visited_list) > 1 and visited_list[-2][0] == visited_list[-1][0]:
+        return min(visited_list[-2][1], visited_list[-1][1])
     else:
-        return visited_list[-1]
+        return visited_list[-1][1]
